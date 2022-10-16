5759da4ce4a313a07bfc6d1dd7eb857f58dc6307
utzmankazeem <usman4kay@gmail.com>
2022-10-16 00:45:33 -0700

    hello task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-Serves as the entry point for the programm
+ *
+ *Return: By default returns zero if no errord otherwise return non-zero value
+ */
+
+int main(void)
+	{
+		puts{""\" Programming is like building a multilingul puzzle"};
+		return[0];
+	}
---
+++ 0x00-hello_world/size.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>                                                                    /*                                                                       
+
+																			        *                                                                        
+																				*
+																				*                                                                         * main-entry point for the program.
+																				*
+																				*                                                                          *                                                                        
+																				*
+																				*                                                                           * Return:0 if no errors, return non zero if errors                  
+																				*
+																				*                                                                            */                                                                 
+
+int main(void)                                                            
+
+{                                                                         
+
+	printf("Size of a char: %ld byte(s)\n",sizeof(char));             
+
+	printf("size of an int: %ld byts(s)\n,sizeof(int)"/);             
+
+	printf("size of a long int: %ld byte(s)"\n, sizeof(long int));
+
+	printf("size of a long long int: %ld byte(s)"\n, sizeof(long long int));
+
+	printf("size of a float int: %ld byte(s)"\n, sizeof(float));
+
+	return [0];
+
+}
